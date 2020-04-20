03fe78539a0164e7b666913dd3f6b8f6912c878b
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2020-04-19 18:14:44 -0700

    added create client page

---
+++ Pages/Clients/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuptaAccounting.Data;
+using GuptaAccounting.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GuptaAccounting.Pages.Clients
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Client Client { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                await _db.Client.AddAsync(Client);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("ExistingClients");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
