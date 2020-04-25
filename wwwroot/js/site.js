662b2eb4d376e164874d5cdd4f69896b6f17b542
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2020-04-25 16:46:53 -0700

    added client-side validation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,11 +3,11 @@
 
 // Write your Javascript code.
 function CheckCheckboxes() {
+    var Tax_Returns = document.getElementById("Client_Tax_Returns").checked;
     var Bookkeeping = document.getElementById("Client_Bookkeeping").checked;
     var Personal_Income_Taxation = document.getElementById("Client_Personal_Income_Taxation").checked;
     var Self_Employed_Business_Taxes = document.getElementById("Client_Self_Employed_Business_Taxes").checked;
     var GST_PST_WCB_Returns = document.getElementById("Client_GST_PST_WCB_Returns").checked;
-    var Tax_Returns = document.getElementById("Tax_Returns").checked;
     var Payroll_Services = document.getElementById("Client_Payroll_Services").checked;
     var Previous_Year_Filings = document.getElementById("Client_Previous_Year_Filings").checked;
     var Government_Requisite_Form_Applications = document.getElementById("Client_Government_Requisite_Form_Applications").checked;
@@ -21,16 +21,12 @@ function CheckCheckboxes() {
         Payroll_Services == false &&
         Previous_Year_Filings == false &&
         Government_Requisite_Form_Applications == false &&
-        Other == null) {
-        alert("hello");
+        Other.value == "") {
         var CheckboxError = document.getElementById("CheckboxError");
         CheckboxError.innerHTML = "At least one checkbox or the 'Other' field must be filled";
         return false;
     }
     else {
-        alert("hello");
-        var CheckboxError = document.getElementById("CheckboxError")
-        CheckboxError.innerHTML = "OK";
         return true;
     }
 };
