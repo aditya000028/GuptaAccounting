baa3bc8d8849e6b340a5b01a5c3286e06330b559
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2020-04-27 17:56:14 -0700

    added table to consultation requests tab. Added form to free consultation tab. Updated site.js to erase dangert text if valid form is submitted

--- Pages/Clients/ConsultationRequests.cshtml.cs
+++ Pages/Clients/ConsultationRequests.cshtml.cs
@@ -2,16 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GuptaAccounting.Data;
+using GuptaAccounting.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace GuptaAccounting.Pages.Clients
 {
     public class ConsultationRequestsModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _db;
 
+        public ConsultationRequestsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Client> Clients { get; set; }
+
+        public async Task OnGet()
+        {
+            Clients = await _db.Client.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var Client_to_delete = await _db.Client.FindAsync(id);
+
+            if (Client_to_delete == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Client.Remove(Client_to_delete);
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("ConsultationRequests");
+            }
         }
     }
 }
--- Pages/FreeConsultation.cshtml.cs
+++ Pages/FreeConsultation.cshtml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GuptaAccounting.Data;
+using GuptaAccounting.Model;
+using GuptaAccounting.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +12,48 @@ namespace GuptaAccounting.Pages
 {
     public class FreeConsultationModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public CheckboxValidation validation;
+
+        public FreeConsultationModel(ApplicationDbContext db)
+        {
+            _db = db;
+            validation = new CheckboxValidation();
+        }
+
+        [BindProperty]
+        public Client Client { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (validation.CheckCheckboxes(Client.Bookkeeping,
+                    Client.Payroll_Services,
+                    Client.Personal_Income_Taxation,
+                    Client.Previous_Year_Filings,
+                    Client.Self_Employed_Business_Taxes,
+                    Client.Tax_Returns,
+                    Client.GST_PST_WCB_Returns,
+                    Client.Government_Requisite_Form_Applications,
+                    Client.Other) == false)
+                return Page();
+
+            if (ModelState.IsValid)
+            {
+                Client.IsConsultationClient = true;
+                await _db.Client.AddAsync(Client);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
