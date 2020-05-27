d1b58ef1808906b65b81bec4cd48296b319fc6f9
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2020-05-27 16:21:05 -0700

    Added server side validation - created utility class. Unable to add client side validation

--- Pages/Clients/Create.cshtml.cs
+++ Pages/Clients/Create.cshtml.cs
@@ -14,12 +14,9 @@ namespace GuptaAccounting.Pages.Clients
     {
         private readonly ApplicationDbContext _db;
 
-        public CheckboxValidation validation;
-
         public CreateModel(ApplicationDbContext db)
         {
             _db = db;
-            validation = new CheckboxValidation();
         }
 
         [BindProperty]
@@ -32,17 +29,6 @@ namespace GuptaAccounting.Pages.Clients
 
         public async Task<IActionResult> OnPost()
         {
-            if (validation.CheckCheckboxes(Client.Bookkeeping,
-                    Client.Payroll_Services,
-                    Client.Personal_Income_Taxation,
-                    Client.Previous_Year_Filings,
-                    Client.Self_Employed_Business_Taxes,
-                    Client.Tax_Returns,
-                    Client.GST_PST_WCB_Returns,
-                    Client.Government_Requisite_Form_Applications,
-                    Client.Other) == false)
-                return Page();
-
             if (ModelState.IsValid)
             {
                 Client.IsConsultationClient = false;
--- Pages/FreeConsultation.cshtml.cs
+++ Pages/FreeConsultation.cshtml.cs
@@ -18,12 +18,9 @@ namespace GuptaAccounting.Pages
     {
         private readonly ApplicationDbContext _db;
 
-        public CheckboxValidation validation;
-
         public FreeConsultationModel(ApplicationDbContext db)
         {
             _db = db;
-            validation = new CheckboxValidation();
         }
 
         [BindProperty]
@@ -234,18 +231,6 @@ namespace GuptaAccounting.Pages
 
         public async Task<IActionResult> OnPost()
         {
-            //Add custom attribute for validation instead of this
-            if (validation.CheckCheckboxes(Client.Bookkeeping,
-                    Client.Payroll_Services,
-                    Client.Personal_Income_Taxation,
-                    Client.Previous_Year_Filings,
-                    Client.Self_Employed_Business_Taxes,
-                    Client.Tax_Returns,
-                    Client.GST_PST_WCB_Returns,
-                    Client.Government_Requisite_Form_Applications,
-                    Client.Other) == false)
-                return Page();
-
             if (ModelState.IsValid)
             {
                 Client.IsConsultationClient = true;
