40729e6c5031a4cfcde6613db39dfd1fa966d1c0
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2020-06-01 17:31:41 -0700

    added email address field to client model

---
+++ Data/Migrations/20200602002852_AddEmailAddressToDB.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuptaAccounting.Data.Migrations
+{
+    public partial class AddEmailAddressToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Client",
+                maxLength: 320,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Client");
+        }
+    }
+}
