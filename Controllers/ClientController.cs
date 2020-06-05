00d770d3404de9824881aad7054106b738cfedc0
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2020-06-04 20:12:01 -0700

    added another table to existingclients page - added js file and controller to use api and enabled api's in solution

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GuptaAccounting.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuptaAccounting.Controllers
+{
+    //Need to add the route so server knows which path/url to go to
+    [Route("api/Client")]
+    //State that this is an api controller
+    [ApiController]
+    public class ClientController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ClientController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new
+            {
+                data = _db.Client.Where(Client => Client.IsConsultationClient == false).ToList()
+            });
+        }
+    }
+}
---
+++ wwwroot/js/LoadDatatable.js
@@ -0,0 +1,80 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    //will populate the data table from the cdn that I have used on id DT_load
+    dataTable = $('#DT_load').DataTable({
+        //need to make an ajax call using the api that i included
+        "ajax": {
+            "url": "/api/client",
+            //this is a get request
+            "type": "GET",
+            "datatype": "json"
+        },
+        //this is an array
+        "columns": [
+            //these are all column names filled with data
+            { "data": "name", "width": "20%" },
+            { "data": "contactNumber", "width": "15%" },
+            { "data": "emailAddress", "width": "20%" },
+            {
+                "data": null,
+                "width": "20%",
+                "render": function (data, type, row) {
+                    var html = [];
+                    if (row.bookkeeping) {
+                        html.push("Bookkeeping");
+                    }
+                    if (row.personal_Income_Taxation) {
+                        html.push("Personal Income Taxation");
+                    }
+                    if (row.self_Employed_Business_Taxes) {
+                        html.push("Self Employed Business Taxes");
+                    }
+                    if (row.gST_PST_WCB_Returns) {
+                        html.push("GST/PST/WCB Returns");
+                    }
+                    if (row.tax_Returns) {
+                        html.push("Tax Returns");
+                    }
+                    if (row.payroll_Services) {
+                        html.push("Payroll Services");
+                    }
+                    if (row.previous_Year_Filings) {
+                        html.push("Previous Year Filings");
+                    }
+                    if (row.government_Requisite_Form_Applications) {
+                        html.push("Government Requisite Form Applications");
+                    }
+                    if (row.other != "") {
+                        html.push(row.other);
+                    }
+                      // continue with all options
+                    return html.join('<br>');
+                }
+            },
+            { "data": "nextStep", "width": "20%" },
+            //the last column is the delete and edit button
+            {
+                //need to pass the id when editing
+                "data": "id",
+                //need to render 2 buttons
+                "render": function (data) { //this data will have the id of the client
+                    //we want to return a div with 2 buttons
+                    return ` <div class="text-center">
+                                <a href="Admin/Edit?id=${data}" class="btn btn-success text-white p-1" style="cursor:pointer;width:90px">Edit</a>
+                                <a class="btn btn-danger text-white p-1" style="width:90px">Delete</a>
+                             </div>`;
+                    //make the render have a width of 30%             
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No existing clients"
+        },
+        "width": "100%"
+    });
+};
